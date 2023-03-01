7c63287e4cd379bdf64517c3b6c34061ad82b4f2
Dahami Dasanayaka <dahamidasanayaka1@gmail.com>
2023-03-01 19:06:32 +0530

    Initial commit

---
+++ src/main/java/com/example/spex1/SpEx1Application.java
@@ -0,0 +1,13 @@
+package com.example.spex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpEx1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpEx1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spex1/SpEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpEx1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
